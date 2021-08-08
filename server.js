13eff1d6ce5ac964ca77436ceda25847a4a7bf6c
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-08 12:35:15 -0400

    updated server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 const db = require("./models");
 
 const app = express();
+
 app.use(logger("dev"));
 
 app.use(
@@ -16,67 +17,14 @@ app.use(
 app.use(express.json());
 
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
-db.workout
-  .create({ name: "Workout Tracker" })
-  .then((dbworkout) => {
-    console.log(dbworkout);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-//need to work on post//
-app.post("/exercise", ({ body }, res) => {
-  db.exercise
-    .create(body)
-    .then(({ _id }) =>
-      db.workout.findOneAndUpdate({}, { $push: { stats: _id } }, { new: true })
-    )
-    .then((dbworkout) => {
-      res.json(dbworkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/exercise", (req, res) => {
-  db.Exercise.find({})
-    .then((dbBook) => {
-      res.json(dbBook);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/library", (req, res) => {
-  db.Library.find({})
-    .then((dbLibrary) => {
-      res.json(dbLibrary);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/populated", (req, res) => {
-  db.Library.find({})
-    .populate("books")
-    .then((dbLibrary) => {
-      res.json(dbLibrary);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.use("./routes/apiRoutes.js");
-app.use("./routes/htmlRoutes.js");
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
