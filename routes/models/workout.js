4b62524a340e2b5eef2cc2063fa299259c5325c2
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-07 20:23:14 -0400

    added to workout.js in models

---
+++ routes/models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Please enter the type of exercise",
+      },
+      name: {
+        type: String,
+        required: "Please enter the name of exercise",
+      },
+      duration: {
+        type: Number,
+        required: "Please enter the number of minutes",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
