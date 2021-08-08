5141a4187465e931b61e76f08439b59d1ef0e7d5
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-07 21:11:53 -0400

    working on server.js post,get,put

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
+const db = require("./models");
+
 const app = express();
+app.use(logger("dev"));
+
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 app.use(express.json());
-app.use(morgan("dev"));
 
 app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/workout", {
@@ -18,9 +21,63 @@ mongoose.connect("mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
+db.workout
+  .create({ name: "Workout Tracker" })
+  .then((dbworkout) => {
+    console.log(dbworkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+//need to work on post//
+app.post("/exercise", ({ body }, res) => {
+  db.exercise
+    .create(body)
+    .then(({ _id }) =>
+      db.workout.findOneAndUpdate({}, { $push: { stats: _id } }, { new: true })
+    )
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/exercise", (req, res) => {
+  db.Exercise.find({})
+    .then((dbBook) => {
+      res.json(dbBook);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/library", (req, res) => {
+  db.Library.find({})
+    .then((dbLibrary) => {
+      res.json(dbLibrary);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/populated", (req, res) => {
+  db.Library.find({})
+    .populate("books")
+    .then((dbLibrary) => {
+      res.json(dbLibrary);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 app.use("./routes/apiRoutes.js");
 app.use("./routes/htmlRoutes.js");
 
 app.listen(PORT, () => {
-  console.log(`App is running on PORT http://localhost:${PORT}`);
+  console.log(`App running on port ${PORT}!`);
 });
