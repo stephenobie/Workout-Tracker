c4e68bc651ccea09440eb58459bd071ab30b1022
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-07 20:18:54 -0400

    added routes and express to server.js

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ app.use(
     extended: true,
   })
 );
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+app.use("./routes/apiRoutes.js");
+app.use("./routes/htmlRoutes.js");
+
+app.listen(PORT, () => {
+  console.log(`App is running on PORT http://localhost:${PORT}`);
+});
