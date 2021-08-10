31b9eb144037acbc6c16c3f6c8d60afd460789c7
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-09 21:04:37 -0400

    update for heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,59 +1,49 @@
-const router = require("express").Router();
-const db = require("../models");
+const router = require('express').Router(); 
+const db = require('../models'); 
 
-router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
+router.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => { res.json(dbWorkout) } )
+    .catch(err => { res.json(err) });
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, 
+        { $push: { exercises: req.body } }, 
+        { new: true, runValidators: true }
+    )
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkouts => {
+        const workouts = dbWorkouts.map(workout => {
+            console.log(workout);
+            const duration = workout.exercises.reduce((acc,next) => {
+                return acc + next.duration;
+            }, 0);
+
+            return {
+                
+                totalDuration: duration, 
+                ...workout.toObject()
+            }
+        })
+        res.json(workouts); 
     })
-    .catch((err) => {
-      res.json(err);
+    .catch(err => {
+        res.json(err); 
     });
 });
 
-router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findByIdAndUpdate(
-    { _id: req.params.id },
-    { $push: { exercises: req.body } },
-    { new: true, runValidators: true }
-  )
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); })
 });
 
-router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({})
-    .then((dbWorkouts) => {
-      const workouts = dbWorkouts.map((workout) => {
-        console.log(workout);
-        const duration = workout.exercises.reduce((acc, next) => {
-          return acc + next.duration;
-        }, 0);
-
-        return {
-          totalDuration: duration,
-          ...workout.toObject(),
-        };
-      });
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  // useFindAndModify: false,
-  // useCreateIndex: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
